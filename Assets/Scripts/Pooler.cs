2928cdfc19361567c1df2e2fb44f8535749e053f
Furion <artemfurletov6@gmail.com>
2020-11-29 23:45:39 +0200

    spawner

---
+++ Assets/Scripts/Pooler.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class Pooler
+{
+    private static Dictionary<string,Pool> pools = new Dictionary<string, Pool>();
+
+    public static GameObject Spawn(GameObject prefab, Vector3 position, Quaternion rotation)
+    {
+        GameObject obj;
+        var key = prefab.name.Replace("(Clone)", "");
+
+        if (pools.ContainsKey(key))
+        {
+            if (pools[key].inactive.Count == 0)
+            {
+                obj = Object.Instantiate(prefab, position, rotation,pools[key].parent.transform);
+            }
+            else
+            {
+                obj = pools[key].inactive.Pop();
+                obj.transform.position = position;
+                obj.transform.rotation = rotation;
+                obj.SetActive(true);
+            }
+        }
+        else
+        {
+            GameObject newParent = new GameObject($"{key}_POOL");
+            obj = Object.Instantiate(prefab, position, rotation, newParent.transform);
+            Pool newPool = new Pool(newParent);
+            pools.Add(key,newPool);
+        }
+
+        return obj;
+    }
+    
+    public static void Despawn(GameObject prefab)
+    {
+        var key = prefab.name.Replace("(Clone)", "");
+        if (pools.ContainsKey(key))
+        {
+            pools[key].inactive.Push(prefab);
+            prefab.transform.position = pools[key].parent.transform.position;
+            prefab.SetActive(false);
+        }
+        else
+        {
+            GameObject newParent = new GameObject($"{key}_POOL");
+            Pool newPool = new Pool(newParent);
+            
+            prefab.transform.SetParent(newParent.transform);
+            
+            pools.Add(key,newPool);
+            pools[key].inactive.Push(prefab);
+            prefab.SetActive(false);
+        }
+    }
+
+    public static void DestroyPools()
+    {
+        foreach (var pool in pools.ToList())
+        {
+            Debug.Log(pool.Key + " "+ pool.Value);
+            pools.Remove(pool.Key);
+            Object.Destroy(GameObject.Find($"{pool.Key}_POOL"));
+        }
+    }
+    
+    public static void DespawnAllPools()
+    {
+        foreach (var pool in pools.ToList())
+        {
+            Debug.Log(pool.Key + " "+ pool.Value);
+            //pools.Remove(pool.Key);
+            var parent = GameObject.Find($"{pool.Key}_POOL").transform;
+            var childCount = parent.childCount;
+            for (int i = 0; i < childCount; i++)
+            {
+                Despawn(parent.GetChild(i).gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Cinemachine;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    [Header("Player")]
+    public Transform playerSpawn;
+    public GameObject playerPrefab;
+    [Header("Enemy")]
+    public Transform[] enemySpawns;
+
+    public GameObject enemyPrefab;
+
+    public int enemiesLeft;
+
+    [SerializeField] private int _countToSpawn = 1;
+
+    private CinemachineFreeLook _camera;
+    private void Start()
+    {
+        _camera = FindObjectOfType<CinemachineFreeLook>();
+    }
+
+    private void Update()
+    {
+        if(enemiesLeft != 0) return;
+        SpawnEnemy(_countToSpawn);
+    }
+
+    private void SpawnEnemy(int countToSpawn)
+    {
+        for (int i = 0; i < countToSpawn; i++)
+        {
+            var randomSpawnPosition = Random.Range(0, enemySpawns.Length);
+            Pooler.Spawn(enemyPrefab, enemySpawns[randomSpawnPosition].position, Quaternion.identity);
+        }
+
+        enemiesLeft = countToSpawn;
+
+        if (_countToSpawn % 2 == 0)
+            _countToSpawn += 2;
+        else _countToSpawn++;
+    }
+    
+    [ContextMenu("SpawnPlayer")]
+    public void SpawnPlayer()
+    {
+        Pooler.DespawnAllPools();
+        //get player from pool
+        var instance = Pooler.Spawn(playerPrefab, playerSpawn.position, Quaternion.identity);
+        //restore health to max HP
+        instance.GetComponent<Health>().RestoreHealth();
+        //follow 
+        _camera.Follow = instance.transform;
+        _camera.LookAt = instance.transform;
+    }
+
+    private void ReduceEnemiesCount()
+    {
+        enemiesLeft--;
+    }
+
+    private void OnEnable()
+    {
+        EnemyHealth.OnEnemyDie += ReduceEnemiesCount;
+    }
+    
+    private void OnDisable()
+    {
+        EnemyHealth.OnEnemyDie -= ReduceEnemiesCount;
+    }
+}
