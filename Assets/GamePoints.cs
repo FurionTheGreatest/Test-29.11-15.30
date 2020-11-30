f6412e243544ef1416cd71e3aa07ca6f877dc884
Furion <artemfurletov6@gmail.com>
2020-11-30 02:17:57 +0200

    calculate points for killing enemy

---
+++ Assets/GamePoints.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+
+public class GamePoints : MonoBehaviour
+{
+    public TMP_Text points;
+    private int pointCount = 0;
+
+    public void AddPoints()
+    {
+        pointCount += 100;
+        points.text = pointCount.ToString(CultureInfo.CurrentCulture);
+    }
+    private void OnEnable()
+    {
+        EnemyHealth.OnEnemyDie += AddPoints;
+    }
+    
+    private void OnDisable()
+    {
+        EnemyHealth.OnEnemyDie -= AddPoints;
+    }
+}
